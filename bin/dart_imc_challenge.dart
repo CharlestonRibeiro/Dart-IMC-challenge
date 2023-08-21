85e0f8f564e76be61b900c8c73aaae2224e1e14a
charleston <charles1235223@gmail.com>
2023-08-21 11:42:33 -0400

    first commit

---
+++ bin/dart_imc_challenge.dart
@@ -0,0 +1,5 @@
+import 'package:dart_imc_challenge/dart_imc_challenge.dart' as dart_imc_challenge;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_imc_challenge.calculate()}!');
+}

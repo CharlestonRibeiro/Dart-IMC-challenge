51cb1248cf6115f5f59dcd0d5dde506235bb14c9
charleston <charles1235223@gmail.com>
2023-08-21 15:24:38 -0400

    feat - service calculate IMC

--- lib/src/controllers/panel_controllers.dart
+++ lib/src/controllers/panel_controllers.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
+import 'package:dart_imc_challenge/src/services/imc_service.dart';
+import 'package:dart_imc_challenge/src/models/person_model.dart';
 
 class PanelControllers {
+  final imcService = ImcService();
   int choice = 3;
   String menu = '''
             Painel de controle:
@@ -18,10 +21,27 @@ class PanelControllers {
         case 0:
           print('.:FIM:.');
           break;
-
         case 1:
+          try {
+            print('Digite seu nome:');
+            final name = stdin.readLineSync()!;
+            print('Digite o peso em quilogramas (ex 70):');
+            final weight = double.parse(stdin.readLineSync()!);
+            print('Digite sua altura (ex 1.75):');
+            final height = double.parse(stdin.readLineSync()!);
+            final personModel =
+                PersonModel(name: name, height: height, weight: weight);
+            final imc = imcService.calculateIMC(personModel: personModel);
+            print('');
+            print('Resultado:');
+            print(imcService.interpretIMC(imc: imc, name: name));
+            print('');
+          } catch (e) {
+            print('Erro no que foi digitado!');
+            print('');
+            print('Tente novamente');
+          }
           break;
-
         default:
           print('Escolha invalida!');
           print('');
